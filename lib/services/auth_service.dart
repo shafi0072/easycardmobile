3b8fe02c90d59934dbdd8ad55b4e8d6a6217fd25
shafi0072 <safi29317@gmail.com>
2023-10-24 17:27:50 +0600

    s

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken, idToken: gAuth.idToken);
+    print("Access Token: ${gAuth.accessToken}");
+    await Firebase.initializeApp();
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+}
